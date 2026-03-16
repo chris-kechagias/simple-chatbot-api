2237464b1c3ae0573592bfa061443b868e29b1e1
Chris Kechagias <ck.chris.kechagias@gmail.com>
2026-03-17 00:32:51 +0200

    add config

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ Application launcher.
 Creates the FastAPI app, registers routers, and manages the database lifespan.
 """
 
-#TODO: logger = logging.getLogger(__name__)
-#TODO: lifespan
+# TODO: logger = logging.getLogger(__name__)
+# TODO: lifespan
 
 from fastapi import FastAPI
 
+app = (
+    FastAPI()
+)  # TODO: title, description, version, lifespan, swagger_ui_parameters, etc.
 
-app = FastAPI() #TODO: title, description, version, lifespan, swagger_ui_parameters, etc.
+# TODO: app include routers here
 
-#TODO: app include routers here
-
-#TODO: error handlers here
+# TODO: error handlers here
