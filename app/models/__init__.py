fddedf7876ccf63b43668dc6e03ef3f159cd19f4
Chris Kechagias <ck.chris.kechagias@gmail.com>
2026-03-18 00:12:02 +0200

    add chat models

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .chat import ChatRequest as ChatRequest
+from .chat import ChatResponse as ChatResponse
+from .chat import Conversation as Conversation
+from .chat import Message as Message
+from .health import HealthResponse as HealthResponse
