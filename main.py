be759fa4f48da36c1eedae1313f71bc32800fc29
Chris Kechagias <ck.chris.kechagias@gmail.com>
2026-03-19 23:38:46 +0200

    register exception handlers in main

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ Application launcher.
 Creates the FastAPI app, registers routers, and manages the database lifespan.
 """
 
-# TODO: logger = logging.getLogger(__name__)
 # TODO: lifespan
+# Standard Library Imports
+import logging
+
 # Third-Party Imports
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 
 # Local/First-Party Imports
 from app.config import config
+from app.middleware import (
+    app_exception_handler,
+    generic_exception_handler,
+    setup_logging,  # noqa: F401,
+    validation_exception_handler,
+)
+from app.utils.errors import AppException
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI(
     title=config.app_name,
@@ -20,4 +32,6 @@ app = FastAPI(
 
 # TODO: app include routers here
 
-# TODO: error handlers here
+app.add_exception_handler(AppException, app_exception_handler)
+app.add_exception_handler(Exception, generic_exception_handler)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
