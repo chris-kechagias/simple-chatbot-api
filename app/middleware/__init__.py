2baeb91e08052e451233ddf4db98f863cd249b3d
Chris Kechagias <ck.chris.kechagias@gmail.com>
2026-03-19 23:38:29 +0200

    update middleware init exports

--- app/middleware/__init__.py
+++ app/middleware/__init__.py
@@ -1,1 +1,13 @@
+from .exception_handlers import (
+    app_exception_handler as app_exception_handler,
+)
+from .exception_handlers import (
+    create_error_response as create_error_response,
+)
+from .exception_handlers import (
+    generic_exception_handler as generic_exception_handler,
+)
+from .exception_handlers import (
+    validation_exception_handler as validation_exception_handler,
+)
 from .logger_config import setup_logging as setup_logging
