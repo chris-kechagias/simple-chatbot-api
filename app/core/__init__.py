e13af22988352735853febecad15f606e7507bd3
Chris Kechagias <ck.chris.kechagias@gmail.com>
2026-03-23 21:26:12 +0200

    fix invalid init exports

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,11 +1,11 @@
 from .config import config
-from .database import SessionDep, create_db_and_tables, get_db
+from .database import SessionDep, create_db_and_tables, get_session
 from .logging import setup_logging
 
 __all__ = [
     "setup_logging",
     "config",
     "create_db_and_tables",
-    "get_db",
+    "get_session",
     "SessionDep",
 ]
